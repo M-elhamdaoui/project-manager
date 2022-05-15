b2ad319a91ba88e6928f6ad9e16f371b2bc8e62f
melhamdaoui811 <melhamdaoui811@gmail.com>
2022-05-15 01:04:30 +0100

    Create rooting

--- src/App.js
+++ src/App.js
@@ -1,12 +1,39 @@
 import React from 'react'
 import "./App.css"
+import {Route,BrowserRouter,Switch} from "react-router-dom"
+import {Dashboard,Login,Signup,Create,Project }from "./Pages/index"
+import Navbar from './Components/NavBar/Navbar'
+import Sidebar from './Components/Sidebar/Sidebar'
 
 function App() {
   return (
-    <div>
-        Hello React ! 
+    <div className='App '>
+      <BrowserRouter>
+        <Sidebar />
+        <div className='container'>
+          <Navbar />
+
+          <Switch>
+            <Route path='/' exact>
+              <Dashboard />
+            </Route>
+            <Route path='/login'>
+              <Login />
+            </Route>
+            <Route path='/signup'>
+              <Signup />
+            </Route>
+            <Route path='/projects/:id'>
+              <Project />
+            </Route>
+            <Route path='/create'>
+              <Create />
+            </Route>
+          </Switch>
+        </div>
+      </BrowserRouter>
     </div>
-  )
+  );
 
 }
 
--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
-import App from './App';
-import { AuthContextProvider } from './Context/AuthContext';
-import "./index.css"
-import { createRoot } from "react-dom/client";
-const container = document.getElementById("root");
-const root = createRoot(container); // createRoot(container!) if you use TypeScript
-root.render(
-  <AuthContextProvider>
-    <App />
-  </AuthContextProvider>
-);
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { AuthContextProvider } from "./Context/AuthContext";
+
+
+
+ReactDOM.render(<AuthContextProvider><App/></AuthContextProvider>,document.getElementById("root"))
