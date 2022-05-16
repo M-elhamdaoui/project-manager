65fc817ac4d005e80fbaff95b6bddea01393233f
melhamdaoui811 <melhamdaoui811@gmail.com>
2022-05-16 01:07:48 +0100

    edit auth context adding auth is ready

--- src/Context/AuthContext.js
+++ src/Context/AuthContext.js
@@ -1,4 +1,6 @@
 import { createContext , useReducer } from "react";
+import { auth } from "../Firebase/config";
+import { useEffect } from "react";
 
 export const authReducer=(state,action)=>{
     switch(action.type){
@@ -6,6 +8,8 @@ export const authReducer=(state,action)=>{
             return {...state,user:action.payload}
         case "LOG_OUT":
             return {...state,user:null}
+        case "AUTH_IS_READY":
+            return {...state,user:action.payload,authIsReady:true}
         default:
             return state
     }
@@ -17,8 +21,15 @@ export const AuthContext =createContext()
 export const AuthContextProvider=({children})=>{
 
     const [state , dispatch]=useReducer(authReducer,{
-        user:null
+        user:null,
+        authIsReady:false
     })
+    useEffect(()=>{
+        const unsub=auth.onAuthStateChanged((user)=>{
+            dispatch({type:"AUTH_IS_READY",payload:user})
+            unsub()
+        })
+    },[])
 console.log("authContext state",state)
     return (
         <AuthContext.Provider value={{...state,dispatch}} >
